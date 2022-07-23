39f38cf576602d9ed6b2b4b351eb18d8dc67ddb9
Youen Chéné <youen.chene@gadz.org>
2022-07-23 15:00:03 +0200

    feat : Hover help + run in uae using mono (for fsuae on macos)

--- src/extension.ts
+++ src/extension.ts
@@ -1,64 +1,87 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as xml2js from 'xml2js';
+var path = require("path");
+
+
+
+const BB2_MODE: vscode.DocumentFilter = { language: 'abb2', scheme: 'file' };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
+
+    
+    let xml=fs.readFileSync(context.extensionPath+'/resources/bb2doc.xml','utf8');
+    let bb2doc=[];
+    xml2js.parseString(xml, function (err, result) {
+        if (result != undefined) {
+            result.bb2doc.command.forEach((val) => {
+                bb2doc[val.$.name]=val;
+            });
+            vscode.window.showInformationMessage('Blitz Basic 2 documentation Loaded');
+        }
+      });
+
+
+
 	console.log('Congratulations, your extension "Amiga Blitz Basic 2" is now active!');
+
+
+    vscode.languages.registerHoverProvider('abb2', {
+        provideHover(document, position, token) {
+
+            const range = document.getWordRangeAtPosition(position);
+            const word = document.getText(range);
+
+            if (bb2doc != undefined) {
+                let command=bb2doc[word];
+                if (command != undefined) {
+                   
+                    const mds = new vscode.MarkdownString();
+                    mds.appendMarkdown('<span style="color:#5471C9;">'+ command.keyword.toString()+ '<span>');
+                    if (command.parameters.toString().substring(0,2) != '\r\n') {
+                        mds.appendMarkdown(' <span style="color:#9E622C;">'+command.parameters.toString()+'<span>');
+                    }
+                    mds.appendMarkdown('\n\n');
+                    mds.appendMarkdown(command.longDescription.toString());
+                    mds.isTrusted = true;
+                    return new vscode.Hover(mds);
+                }
+            }
+        }
+    });
 
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let runwinuae = vscode.commands.registerCommand('amiga-blitzbasic2.runwinuae', () => {
-        let path = getPath();
-        if(path.length > 0)
+        let file = getCurrentFile();
+        if(file.length > 0)
         {
-            vscode.window.showInformationMessage('Running in WinUAE...');
+            vscode.window.showInformationMessage('Running in UAE...');
             const editor = vscode.window.activeTextEditor;
             const terminal = vscode.window.createTerminal(`Ext Terminal`);
             terminal.show();
-            terminal.sendText(path+"run.bat");
+            terminal.sendText("mono WinUAEArexx.exe S:blitzbasic2.rexx 1000 \"SharedCode:"+file+"\"");
         }
 	});
 	context.subscriptions.push(runwinuae);
 
     let runamiga = vscode.commands.registerCommand('amiga-blitzbasic2.runamiga', () => {
-        let path = getPath();
+        let path = getCurrentFile();
         if(path.length > 0)
         {
             vscode.window.showInformationMessage('Running on Amiga...');
             const editor = vscode.window.activeTextEditor;
             const terminal = vscode.window.createTerminal(`Ext Terminal`);
             terminal.show();
-            terminal.sendText(getPath()+"run_amiga.bat");
+            terminal.sendText(path+"run_amiga.bat");
         }
-	});
-	context.subscriptions.push(runamiga);
-
-    let showhelp = vscode.commands.registerCommand('amiga-blitzbasic2.showhelp', () => {
-        const editor = vscode.window.activeTextEditor;
-        const terminal = vscode.window.createTerminal(`Ext Terminal`);
-        if(editor === undefined)
-        {
-            vscode.window.showInformationMessage("Nothing to show help for...");
-        }
-        else
-        {
-            let cursorPosition = editor.selection.start;
-            let wordRange = editor.document.getWordRangeAtPosition(cursorPosition);
-            let highlight = editor.document.getText(wordRange);
-
-            if(wordRange?.isSingleLine && highlight.length > 0 )
-            {
-                terminal.show();
-                terminal.sendText("BB2Doc " + highlight);
-            }
-            else
-                vscode.window.showInformationMessage("Nothing to show help for...");
-        }    
 	});
 	context.subscriptions.push(runamiga);
 
@@ -73,31 +96,25 @@ export function activate(context: vscode.ExtensionContext) {
 // this method is called when your extension is deactivated
 export function deactivate() {}
 
-function getPath() : string {
-    let path: string;
-    if (!vscode.workspace.workspaceFolders) {
-        if(vscode.workspace.rootPath === undefined) {
-            if(vscode.window.activeTextEditor === undefined)
-            {
-                path = "";
-            }
-            else
-            {
-                path = vscode.window.activeTextEditor.document.uri.fsPath;
-                path = path.substring(0,path.lastIndexOf("\\")+1);
-            }          
-        }
-        else
-            path = vscode.workspace.rootPath;
-    } else {
+
+function getCurrentFile() : string {
+    let currentlyOpenTabfilePath: string;
+    let currentlyOpenTabfileName: string;
+    let out: string;
+    if((vscode.window.activeTextEditor != undefined) && (vscode.workspace.workspaceFolders))
+    {
+        currentlyOpenTabfilePath = vscode.window.activeTextEditor.document.fileName;
+        currentlyOpenTabfileName = path.basename(currentlyOpenTabfilePath);
         let root: vscode.WorkspaceFolder;
         root = vscode.workspace.workspaceFolders[0];
-        path = root.uri.fsPath;
-    }
-    if(!path.endsWith("\\") && path.length > 0)
-        path += "\\";
+        out = root.uri.fsPath;
 
-    return path;
+        currentlyOpenTabfileName=currentlyOpenTabfilePath.substring(out.length+1,currentlyOpenTabfilePath.length);   
+    return currentlyOpenTabfileName;
+    }
+    else {
+        return "";
+    }
 }
 
 class ABB2DocumentSymbolProvider implements vscode.DocumentSymbolProvider {
